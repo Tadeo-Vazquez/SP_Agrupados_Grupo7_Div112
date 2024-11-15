942921d4e23d4c261fbf4b3a778004490209b09f
Tadeo-Vazquez <tadeovazquez2006@gmail.com>
2024-11-15 15:40:15 -0300

    seguimos modularizando

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-numeros = 2,4
-print(numeros)
+from paquete.funciones_generales import *
+
+def jugar_agrupados()->None:
+    vidas_nivel = 3
+    reinicios = 3
+    while True:
+        
+
+
+
+        break
+
+
+
+
+
+
+    pass    
